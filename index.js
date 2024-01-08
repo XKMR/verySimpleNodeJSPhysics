e69011b1f17e136c9b7bba2fa4909b40699a375c
kamyar <58393719+XKMR@users.noreply.github.com>
2024-01-08 22:48:02 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 const stepTime = 0.03;            //change the precision of the simulation, lower is more  precise. seconds/step
 const mass = 10;                  //Kg - doesn't matter for this version
-const surfaceEnergyReturn = 0.500;  //how much of the velocity is maintained when a bouncing
+const surfaceEnergyReturn = 0.600;//how much of the velocity is maintained when a bouncing
 const gravity = 9.8;              //gravity in m/s^2
-const speedRegulation = 20;     //limit the speed of the simulation - zero for no regulation
+const speedRegulation = 20;       //limit the speed of the simulation - zero for no regulation
 const totalStepGoal = 40000;      //total number of steps to be done if the useElimit is false
 const startPosition = 10          //starting height
-const lowerElimit = 3;            //the energy level to reach to end the simulation if useElimit is true
+const lowerElimit = 10;           //the energy level to reach to end the simulation if useElimit is true
 const useElimit = true;           //weather use the energy level or step count as a way to stop the simulation
 const doDraw = true;              //weather to draw the object on the terminal
 const drawingScale = 6            //to scale the drawing (used to prevent the object from going to the next line of the terminal)
@@ -67,7 +67,7 @@ function draw(position, V, scale){
     var plot = "";
     if(V < 0){
         for(var i=0; i<emptySpaces; i+=1){
-            if(i >= (position + V)*scale) plot += "<";
+            if(i >= (position + V/scale)*scale) plot += "<";
             else plot += " ";
         }
         plot += "X"
