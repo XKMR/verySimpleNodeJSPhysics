d51e12ca4f32264ab55c94ea4c8e6168f2351c4a
kamyar <58393719+XKMR@users.noreply.github.com>
2024-01-09 01:47:11 +0330

    Update index.js
    added speed vector display and etc.

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const fs = require('fs');
 
 const stepTime = 0.03;            //change the precision of the simulation, lower is more  precise. seconds/step
 const mass = 10;                  //Kg - doesn't matter for this version
-const surfaceEnergyReturn = 0.65;  //how much of the velocity is maintained when a bouncing
+const surfaceEnergyReturn = 0.65; //how much of the velocity is maintained when a bouncing
 const gravity = 9.8;              //gravity in m/s^2
-const speedRegulation = 20;     //limit the speed of the simulation - zero for no regulation
+const speedRegulation = 20;       //limit the speed of the simulation - zero for no regulation
 const totalStepGoal = 40000;      //total number of steps to be done if the useElimit is false
 const startPosition = 20          //starting height
-const lowerElimit = 10;            //the energy level to reach to end the simulation if useElimit is true
+const lowerElimit = 10;           //the energy level to reach to end the simulation if useElimit is true
 const useElimit = true;           //weather use the energy level or step count as a way to stop the simulation
 const doDraw = true;              //weather to draw the object on the terminal
 const drawingScale = 5            //to scale the drawing (used to prevent the object from going to the next line of the terminal)
 const reportE = true;             //weather to report the E value of the object in the terminal
 const reportX = true;             //weatehr to report the X value of the object in the terminal
 const reportV = true;             //weatehr to report the V value of the object in the terminal
-const recordHistory = true        //weather to make files
-const startingVelocity = 10        //starting velocity (negative is downwards)
-const historySeparator = "\n"     //character to separate values whilst saving the history
+const recordHistory = true;       //weather to make files
+const startingVelocity = 10;      //starting velocity (negative is downwards)
+const historySeparator = "\n";    //character to separate values whilst saving the history
 
 const creatUpperWall = false
 const upperWallPosition = 40
@@ -35,6 +35,39 @@ var position = startPosition;
 // var direction = 0 (maybe when 2D version is made)
 
 
+
+setInterval(function(){
+    [position, velocity] = step(position, velocity, surfaceEnergyReturn, gravity, stepTime)
+    if(reportE) E = (position*mass*gravity)+(mass*velocity*velocity*(1/2));
+    if(recordHistory){
+        Ehistory[x] = E;
+        Vhistory[x] = velocity;
+        xhistory[x] = position;
+    }
+    x += 1;
+    
+
+    if(x == totalStepGoal && useElimit == false){
+        end(xhistory);
+    }
+    if(Math.round(E) <= lowerElimit && useElimit == true){
+        end(xhistory);
+    }
+
+    console.clear();
+    if(!useElimit) console.log(generateBar(x, totalStepGoal, 60)+x+"/"+totalStepGoal);
+    if(doDraw) console.log(draw(position, velocity, drawingScale));
+    if(reportX) console.log(`x = ${position}`)
+    if(reportV) console.log(`V = ${velocity}`);
+    if(reportE) console.log(`E = ${E}`);
+
+},speedRegulation)//step time
+
+
+fs.writeFileSync('x.txt', "");
+fs.writeFileSync('E.txt', "");
+fs.writeFileSync("V.txt", "");
+
 function generateBar(current, total, barSize){
     var bar = ""
     var numberOfFilledSpaces = Math.round(current/total * barSize);
@@ -51,8 +84,6 @@ function generateBar(current, total, barSize){
     return bar
 }
 
-fs.writeFileSync('x.txt', "");
-fs.writeFileSync('E.txt', "");
 
 function step(x, V, fr, g, t){
         V = V - (g * t);
@@ -90,32 +121,7 @@ function draw(position, V, scale){
     return plot
 }
 
-setInterval(function(){
-    [position, velocity] = step(position, velocity, surfaceEnergyReturn, gravity, stepTime)
-    if(reportE) E = (position*mass*gravity)+(mass*velocity*velocity*(1/2));
-    if(recordHistory){
-        Ehistory[x] = E;
-        Vhistory[x] = velocity;
-        xhistory[x] = position;
-    }
-    x += 1;
-    
 
-    if(x == totalStepGoal && useElimit == false){
-        end(xhistory);
-    }
-    if(Math.round(E) <= lowerElimit && useElimit == true){
-        end(xhistory);
-    }
-
-    console.clear();
-    if(!useElimit) console.log(generateBar(x, totalStepGoal, 60)+x+"/"+totalStepGoal);
-    if(doDraw) console.log(draw(position, velocity, drawingScale));
-    if(reportX) console.log(`x = ${position}`)
-    if(reportV) console.log(`V = ${velocity}`);
-    if(reportE) console.log(`E = ${E}`);
-
-},speedRegulation)//step time
 
 function end(xhistory){
     const endTime = Date.now();
